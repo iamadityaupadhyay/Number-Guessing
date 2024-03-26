51f6016f977462bacae4413df3b3d20c6ef488b0
Aditya Upadhyay <131547655+iamadityaupadhyay@users.noreply.github.com>
2024-03-27 03:31:40 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+name=["aditya","kajal"]
+display=[]
+choosen_word=random.choice(name)
+print(f"The choosen word is {choosen_word}")
+for i in range(len(choosen_word)):
+    display+="_"
+# user_letter=input("Guess the letter:").lower()
+# for i in range(len(choosen_word)):
+#     letter=choosen_word[i]
+#     if letter==user_letter:
+#         display[i]=user_letter 
+for i in range(len(choosen_word)):
+    user_letter=input("Guess the letter:").lower()
+    letter=choosen_word[i]
+    if letter==user_letter:
+        display[i]=user_letter
+    print(display) 
+for i in range(len(choosen_word)):  
+        if display[i]==choosen_word[i]:
+            flag=1
+        else:flag=2
+if flag:print("You win")
+else:print("You lose")
