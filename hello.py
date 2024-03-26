f64a342eaba091d215d766b57e0afe6e750677ca
Aditya Upadhyay <uaditya219@gmail.com>
2024-03-27 03:55:42 +0530

    hangman-adding

--- hello.py
+++ hello.py
@@ -1,4 +1,62 @@
 import random
+
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 name=["aditya","kajal"]
 display=[]
 choosen_word=random.choice(name)
@@ -9,13 +67,16 @@ for i in range(len(choosen_word)):
 # for i in range(len(choosen_word)):
 #     letter=choosen_word[i]
 #     if letter==user_letter:
-#         display[i]=user_letter 
+#         display[i]=user_letter
+j=6
 for i in range(len(choosen_word)):
     user_letter=input("Guess the letter:").lower()
     letter=choosen_word[i]
     if letter==user_letter:
         display[i]=user_letter
     print(display) 
+    print(stages[j])
+    j-=1
 for i in range(len(choosen_word)):  
         if display[i]==choosen_word[i]:
             flag=1
